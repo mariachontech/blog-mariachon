1352f57aae08b7378ea8ab22da8f0674affc86bb
personal <jjtellezg@gmail.com>
2022-10-29 15:07:28 -0500

    incio del proyecto

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
+
+export default function Home() {
+	return (
+		<div className="container">
+			<Head>
+				<title>Create Next App</title>
+				<meta name="description" content="Mariachon Blog" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		</div>
+	)
+}
