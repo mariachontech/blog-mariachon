865900a4ce4fac90cbfce80f1a730662fbc3d093
personal <jjtellezg@gmail.com>
2022-10-29 16:12:46 -0500

    commit de coolaborador

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 				<meta name="description" content="Mariachon Blog" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<h1 className="text-3xl font-bold underline">Test de coolaborador!</h1>
 		</div>
 	)
 }
